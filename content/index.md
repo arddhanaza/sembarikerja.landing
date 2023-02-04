50a12c7720993cfb81f957967db037f62b51022d
zafarddhana-m <arddhanaza99@gmail.com>
2023-02-04 13:20:31 +0700

    chore: add content base

---
+++ content/index.md
@@ -0,0 +1,3 @@
+# Hello World
+
+This word is hello world
--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ['@nuxt/content'],
 
   css: [
     '@/assets/css/main.css',
